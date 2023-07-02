b5ca859cc57e358ae7e87ffd68238dddb62b54ea
peerasak_b <peerasak.boonjaroensook@gmail.com>
2023-07-02 22:10:03 +0700

    init project

---
+++ shortener-url/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="header-name">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.header-name{
+  font-size: 32px;
+  vertical-align: middle;
+}
+</style>
